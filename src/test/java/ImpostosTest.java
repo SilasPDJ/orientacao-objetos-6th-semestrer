8b45c7ca76eed90e167fb895a5ebe5222ff0476e
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-09-07 00:40:56 -0300

    teste de estado federativo é ou não valido concluido

--- src/test/java/ImpostosTest.java
+++ src/test/java/ImpostosTest.java
@@ -5,18 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ImpostosTest {
     Impostos impostosObj;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         impostosObj = new Impostos();
     }
+
     @Test
-    void TestAleatorio(){
-        impostosObj.setValor(200);
-        impostosObj.setEstado("SP");
-        double b = impostosObj.calcularImposto();
+    void deveValidar27EstadosComBaseNoArrayEstadosPermitidos() {
+        assertEquals(27, impostosObj.estadosPermitidos.length);
+        for (String estado : impostosObj.estadosPermitidos) {
+            assertDoesNotThrow(() -> {
+                impostosObj.setEstado(estado);
+            });
+        }
 
-        System.out.println(b);
+    }
 
+    @Test
+    void deveRetornarEstadoInvalidoComBaseNoMetodoSetEstado() {
+        try {
+            impostosObj.setEstado("SPFC");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("estado deve ser da republica federativa brasileira",
+                    e.getMessage());
+        }
 
     }
 
